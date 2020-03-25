c6a5d16a2535168e90018dbd1b66e16715c3a36f
Brock Brown <43049440+Brownbr32@users.noreply.github.com>
2020-03-25 17:01:17 -0600

    here you go Mariah! linear stuff done!

--- linReg/lin_bit.m
+++ linReg/lin_bit.m
@@ -2,7 +2,8 @@
 clc; clear all; close all;
 which = 1:10
 graph = 1
-model = 1:13;
+models = 1:7;
+leg = {};
 
 
 
@@ -11,19 +12,37 @@ price = readmatrix("../data/train.csv");
 t = price(:,1)';
 t = t-42843;
 price = price(:,3)';
+%t = flip(t);
 
 if graph == 1
     plot(t,price); grid
+    leg{length(leg)+1} = "data";
     hold on;
 end
 
 %% Executing:
 
-funy = zeros(max(model),max(model)+1);
-r2 = zeros(length(model),1);
-for model = model
-    [funy(model,end-model:end),r2(model)] = polyReg(model,t,y);
+funy = zeros(max(models),max(models)+1);
+r2 = zeros(length(models),1);
+for model = models
+    [funy(model,end-model:end),r2(model)] = polyReg(model,t,price);
+    leg{length(leg)+1} = num2str(model);
 end
+x = zeros(length(t),max(models)+1);
+for i = 0:max(models)
+    x(:,end-i) = t.^i;
+end
+
+disp(funy);
+
+out = x*funy';
+if graph == 1
+    plot(t,out);
+end
+%% Comparing
+
+legend(leg)
+
 
 
 
